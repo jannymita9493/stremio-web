716799b76e47193e0bfc6d62e14e314756c8c7f2
Janny Mita <jannymita9493@gmail.com>
2025-02-28 21:14:00 +0700

    Update http_server.js

--- http_server.js
+++ http_server.js
@@ -6,18 +6,26 @@ const INDEX_CACHE = 7200;
 const ASSETS_CACHE = 2629744;
 const PORT = String(process.env.PORT || 8080);
 
-const express = require('express');
-const path = require('path');
+const express = require("express");
+const path = require("path");
 
-const build_path = path.resolve(__dirname, 'build');
-const index_path = path.join(build_path, 'index.html');
+const build_path = path.resolve(__dirname, "build");
+const index_path = path.join(build_path, "index.html");
 
-express().use(express.static(build_path, {
-    setHeaders: (res, path) => {
-        if (path === index_path) res.set('cache-control', `public, max-age: ${INDEX_CACHE}`);
-        else res.set('cache-control', `public, max-age: ${ASSETS_CACHE}`);
-    }
-})).all('*', (_req, res) => {
+express()
+  .use(
+    express.static(build_path, {
+      setHeaders: (res, path) => {
+        if (path === index_path)
+          res.set("cache-control", `public, max-age: ${INDEX_CACHE}`);
+        else res.set("cache-control", `public, max-age: ${ASSETS_CACHE}`);
+      },
+    })
+  )
+  .all("*", (_req, res) => {
     // TODO: better 404 page
-    res.status(404).send('<h1>404! Page not found</h1>');
-.listen(PORT, '0.0.0.0', () => console.info(`Server listening on port: ${PORT}`));
+    res.status(404).send("<h1>404! Page not found</h1>");
+  })
+  .listen(PORT, "0.0.0.0", () =>
+    console.info(`Server listening on port: ${PORT}`)
+  );
